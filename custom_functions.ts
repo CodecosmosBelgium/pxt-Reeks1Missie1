a827e7bf9f854f7c2dc3637803d897b54da0cccb
SebastiaanMoons <sebastiaan.moons@hotmail.com>
2023-07-26 12:53:31 +0200

    Update pxt.json, main.blocks, custom_functions.ts, custom_functions_EN.ts

--- custom_functions.ts
+++ custom_functions.ts
@@ -1,15 +1,15 @@
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
-    let order = 0;
-    let lastPlaced = 0;
+    let order = -1;
+    let lastPlaced = -1;
 
-    //% block="zetKlaar"
+    //% block="zetAgentKlaar"
     export function startingPosition() {
         agent.teleport(world(87, 72, -28), EAST);
         player.execute("scoreboard players set @a woolDetected 0");
     }
 
-    //% block="ga naar volgend level"
+    //% block="gaNaarVolgendLevel"
     export function goToNextLevel() {
         player.execute("execute @a[scores={progress=10..,ex1_lvl=1..6,ex1_move=0,woolDetected=0}] ~ ~ ~ execute @e[type=agent] ~ ~ ~ detect ~ ~-1 ~ gold_block 1 scoreboard players set @a ex1_move 1");
     }
@@ -20,15 +20,19 @@ namespace CodeCosmos {
             lastPlaced++;
             player.execute(
                 "function exercises/parts/top"
-            )
+            );
             order++;
             loops.pause(4000);
         } else {
             player.execute(
                 'titleraw @a title {"rawtext":[{"translate":"title.subtitle.not.correct"}]}'
-            )
+            );
+            player.execute(
+                'tellraw @a {"rawtext":[{"translate":"text.not.correct.buildTop"}]}'
+            );
+            order = -1;
+            lastPlaced = -1;
         }
-
     }
 
     //% block="bouwOpslagplaats"
@@ -37,13 +41,18 @@ namespace CodeCosmos {
             lastPlaced++;
             player.execute(
                 "function exercises/parts/storage"
-            )
+            );
             order++;
             loops.pause(4000);
         } else {
             player.execute(
                 'titleraw @a title {"rawtext":[{"translate":"title.subtitle.not.correct"}]}'
-            )
+            );
+            player.execute(
+                'tellraw @a {"rawtext":[{"translate":"text.not.correct.buildWarehouse"}]}'
+            );
+            order = -1;
+            lastPlaced = -1;
         }
     }
 
@@ -53,13 +62,18 @@ namespace CodeCosmos {
             lastPlaced++;
             player.execute(
                 "function exercises/parts/living_room"
-            )
+            );
             order++;
             loops.pause(4000);
         } else {
             player.execute(
                 'titleraw @a title {"rawtext":[{"translate":"title.subtitle.not.correct"}]}'
-            )
+            );
+            player.execute(
+                'tellraw @a {"rawtext":[{"translate":"text.not.correct.buildLivingRoom"}]}'
+            );
+            order = -1;
+            lastPlaced = -1;
         }
     }
 
@@ -69,24 +83,39 @@ namespace CodeCosmos {
             lastPlaced++;
             player.execute(
                 "function exercises/parts/dining_room"
-            )
+            );
             order++;
             loops.pause(4000);
         } else {
             player.execute(
-                'titleraw @a title {"rawtext":[{"text":"title.subtitle.not.correct","color":"red"}]}'
-            )
+                'titleraw @a title {"rawtext":[{"translate":"title.subtitle.not.correct"}]}'
+            );
+            player.execute(
+                'tellraw @a {"rawtext":[{"translate":"text.not.correct.buildDiningRoom"}]}'
+            );
+            order = -1;
+            lastPlaced = -1;
         }
     }
 
-    //% block="bouwenStarten"
+    //% block="bouwMotoren"
     export function startBuild() {
-        order = 0
-        lastPlaced = 0
-        player.execute(
-            "function exercises/exercise2/reset"
-        )
+        if (lastPlaced === -1) {
+            lastPlaced++;
+            player.execute(
+                "function exercises/exercise2/reset"
+            );
+            order++;
+            loops.pause(4000);
+        } else {
+            player.execute(
+                'titleraw @a title {"rawtext":[{"translate":"title.subtitle.not.correct"}]}'
+            );
+            player.execute(
+                'tellraw @a {"rawtext":[{"translate":"text.not.correct.startBuilding"}]}'
+            );
+            order = -1;
+            lastPlaced = -1;
+        }
     }
-
-
 }
--- custom_functions_EN.ts
+++ custom_functions_EN.ts
@@ -1,7 +1,7 @@
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmosEN {
-    let order = 0;
-    let lastPlaced = 0;
+    let order = -1;
+    let lastPlaced = -1;
 
     //% block="startingPosition"
     export function startingPosition() {
@@ -9,7 +9,7 @@ namespace CodeCosmosEN {
         player.execute("scoreboard players set @a woolDetected 0");
     }
 
-    //% block="go to next level"
+    //% block="goToNextLevel"
     export function goToNextLevel() {
         player.execute("execute @a[scores={progress=10..,ex1_lvl=1..6,ex1_move=0,woolDetected=0}] ~ ~ ~ execute @e[type=agent] ~ ~ ~ detect ~ ~-1 ~ gold_block 1 scoreboard players set @a ex1_move 1");
     }
@@ -20,15 +20,19 @@ namespace CodeCosmosEN {
             lastPlaced++;
             player.execute(
                 "function exercises/parts/top"
-            )
+            );
             order++;
             loops.pause(4000);
         } else {
             player.execute(
                 'titleraw @a title {"rawtext":[{"translate":"title.subtitle.not.correct"}]}'
-            )
+            );
+            player.execute(
+                'tellraw @a {"rawtext":[{"translate":"text.not.correct.buildTop"}]}'
+            );
+            order = -1;
+            lastPlaced = -1;
         }
-
     }
 
     //% block="buildWarehouse"
@@ -37,13 +41,18 @@ namespace CodeCosmosEN {
             lastPlaced++;
             player.execute(
                 "function exercises/parts/storage"
-            )
+            );
             order++;
             loops.pause(4000);
         } else {
             player.execute(
                 'titleraw @a title {"rawtext":[{"translate":"title.subtitle.not.correct"}]}'
-            )
+            );
+            player.execute(
+                'tellraw @a {"rawtext":[{"translate":"text.not.correct.buildWarehouse"}]}'
+            );
+            order = -1;
+            lastPlaced = -1;
         }
     }
 
@@ -53,13 +62,18 @@ namespace CodeCosmosEN {
             lastPlaced++;
             player.execute(
                 "function exercises/parts/living_room"
-            )
+            );
             order++;
             loops.pause(4000);
         } else {
             player.execute(
                 'titleraw @a title {"rawtext":[{"translate":"title.subtitle.not.correct"}]}'
-            )
+            );
+            player.execute(
+                'tellraw @a {"rawtext":[{"translate":"text.not.correct.buildLivingRoom"}]}'
+            );
+            order = -1;
+            lastPlaced = -1;
         }
     }
 
@@ -69,24 +83,39 @@ namespace CodeCosmosEN {
             lastPlaced++;
             player.execute(
                 "function exercises/parts/dining_room"
-            )
+            );
             order++;
             loops.pause(4000);
         } else {
             player.execute(
-                'titleraw @a title {"rawtext":[{"text":"title.subtitle.not.correct","color":"red"}]}'
-            )
+                'titleraw @a title {"rawtext":[{"translate":"title.subtitle.not.correct"}]}'
+            );
+            player.execute(
+                'tellraw @a {"rawtext":[{"translate":"text.not.correct.buildDiningRoom"}]}'
+            );
+            order = -1;
+            lastPlaced = -1;
         }
     }
 
     //% block="startBuilding"
     export function startBuild() {
-        order = 0
-        lastPlaced = 0
-        player.execute(
-            "function exercises/exercise2/reset"
-        )
+        if (lastPlaced === -1) {
+            lastPlaced++;
+            player.execute(
+                "function exercises/exercise2/reset"
+            );
+            order++;
+            loops.pause(4000);
+        } else {
+            player.execute(
+                'titleraw @a title {"rawtext":[{"translate":"title.subtitle.not.correct"}]}'
+            );
+            player.execute(
+                'tellraw @a {"rawtext":[{"translate":"text.not.correct.startBuilding"}]}'
+            );
+            order = -1;
+            lastPlaced = -1;
+        }
     }
-
-
 }
